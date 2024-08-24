aa986010bf1501b9dcae9bcf8181d1579246da78
Brandon T <btai@bayviewglen.ca>
2024-08-24 09:06:07 -0400

    marketplace

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -34,6 +34,7 @@
         </div>
 
         <!-- End Vortex -->
+        <!-- Start FRC -->
         <div class="columns is-vcentered">
             <!-- Image Section -->
             <div class="column is-three-quarters">
@@ -52,6 +53,38 @@
                     Division. </p>
                 <br>
                 <div id="tags-container" class="tags-container">
+                    <span class="tag">Vue</span>
+                    <span class="tag">Express</span>
+                    <span class="tag">MongoDB</span>
+                    <span class="tag">Node</span>
+                    <a href="https://crescendoscoutingapp.onrender.com/" target="_blank">
+                        <span id="gittag" class="tag">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon">
+                                <path
+                                    d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- Start Vendoza -->
+        <div class="columns is-vcentered">
+            <!-- Image Section -->
+            <div class="column is-three-quarters">
+                <img id="vortexAI" src="../assets/vendoza.png" alt="Vortex AI">
+            </div>
+
+            <!-- Text Section -->
+            <div id="leftC" class="column">
+                <p id="headingTwo" class="is-size-5">VENDOZA MARKETPLACE</p>
+
+                <p id="bodyText">An online marketplace application that allows users to purchase and sell items. Used
+                    the MEVN stack to create stunning pages, and store users in the mongoose database </p>
+                <br>
+                <div id="tags-container" class="tags-container">
+                    <span class="tag">Passport.js</span>
+
                     <span class="tag">Vue</span>
                     <span class="tag">Express</span>
                     <span class="tag">MongoDB</span>
@@ -146,13 +179,19 @@
     fill: currentColor;
 }
 
+
+#leftC {
+    transform: translateX(-15%);
+}
+
 @media (max-width: 768px) {
 
 
 
 
     #leftC {
-        transform: translateX(10%);
+        transform: translateX(0%);
+        margin-left: 20px;
     }
 
 
