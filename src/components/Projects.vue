3a565a2941e17c33d9205f0f27d4792dccbe7efa
Brandon T <btai@bayviewglen.ca>
2024-08-23 23:05:56 -0400

    part of project section done

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="intro">
         <p id="headingOne" class="is-size-4">FEATURED PROJECTS</p>
-
         <!-- Vortex AI -->
         <div class="columns is-vcentered">
             <!-- Image Section -->
@@ -10,18 +9,68 @@
             </div>
 
             <!-- Text Section -->
-            <div class="column">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae est culpa, laborum sint autem corrupti
-                    voluptas, ipsum id ab eveniet ea atque deleniti accusamus et, alias temporibus modi aspernatur
-                    impedit.</p>
+            <div id="leftC" class="column">
+                <p id="headingTwo" class="is-size-5">VORTEX AI</p>
+
+                <p id="bodyText">A mobile AI app that combines a language and image model into one easy to use
+                    mobile application. Utilizes the Groq API and HuggingFace StableDiffusion API. </p>
+                <br>
+                <div id="tags-container" class="tags-container">
+                    <span class="tag">React</span>
+                    <span class="tag">ExpoGo</span>
+                    <span class="tag">GroqAPI</span>
+                    <span class="tag">Stable Diffusion API</span>
+                    <a href="https://github.com/BrandonT310442/VortexAI2" target="_blank">
+                        <span id="gittag" class="tag">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="icon">
+                                <!-- SVG Path Data -->
+                                <path
+                                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- End Vortex -->
+        <div class="columns is-vcentered">
+            <!-- Image Section -->
+            <div class="column is-three-quarters">
+                <img id="vortexAI" src="../assets/AltF4 Scouting App.png" alt="Vortex AI">
+            </div>
+
+            <!-- Text Section -->
+            <div id="leftC" class="column">
+                <p id="headingTwo" class="is-size-5">FRC SCOUTING APP</p>
+
+                <p id="bodyText">A scouting app for the First Robotics Competition 2024 Crescendo Season. This app
+                    collects data on robot performance and helps determine the most suitable teams for alliance
+                    selection.
+                    This app led my team to a 70%+ win rate and and First Place in the First Robotics Competition World
+                    Championships Galileo
+                    Division. </p>
+                <br>
+                <div id="tags-container" class="tags-container">
+                    <span class="tag">Vue</span>
+                    <span class="tag">Express</span>
+                    <span class="tag">MongoDB</span>
+                    <span class="tag">Node</span>
+                    <a href="https://crescendoscoutingapp.onrender.com/" target="_blank">
+                        <span id="gittag" class="tag">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon">
+                                <path
+                                    d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
-export default {}
-</script>
+
 
 <style scoped>
 /* Styling for the heading */
@@ -38,11 +87,86 @@ export default {}
     transform: translateX(10%);
 }
 
+#headingTwo {
+    color: #007bff;
+    font-family: "Work Sans", sans-serif;
+    font-weight: 600;
+    letter-spacing: 0.2rem;
+    text-decoration: underline;
+    text-underline-offset: 10px;
+    top: 100px;
+    z-index: 1.0;
+}
+
 /* Image Size */
 #vortexAI {
     margin-top: 15px;
-    margin-left: 100px;
-    width: 666.66px;
-    height: 471.333px;
+    max-width: 100%;
+    transform: scale(0.75);
+    height: auto;
+}
+
+/* Body Text Styling */
+#bodyText {
+    margin-top: 20px;
+}
+
+/* Tags Container Styling */
+.tags-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    /* Space between tags */
+}
+
+/* Tag Styling */
+.tag {
+    background-color: white;
+    color: #007bff;
+    border: 1.5px solid #007bff;
+    padding: 5px 10px;
+    transition: 0.2s ease-in;
+    display: flex;
+    align-items: center;
+}
+
+#gittag {
+    background-color: white;
+    color: #007bff;
+    border: 0px !important;
+    padding: 5px 10px;
+    transition: 0.2s ease-in;
+    display: flex;
+    align-items: center;
+}
+
+/* Tag Hover Effect */
+.tag:hover {
+    opacity: 0.5;
+    transition: 0.2s ease-out;
+    cursor: pointer;
+}
+
+/* SVG Icon Styling */
+.icon {
+    width: 24px;
+    /* Adjust icon size */
+    height: 24px;
+    /* Maintain aspect ratio */
+    fill: currentColor;
+    /* Use current text color */
+}
+
+/* Media Query for screens 768px wide or below */
+@media (max-width: 768px) {
+
+
+
+
+    #leftC {
+        transform: translateX(10%);
+    }
+
+
 }
 </style>
